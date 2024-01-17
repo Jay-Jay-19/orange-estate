02ccdcf5e3014655568f9245600148628875e6e5
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-01-17 10:25:58 +0100

    add comments to sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -17,14 +17,23 @@ export const signup = async (req, res, next) => {
 };
 
 export const signin = async (req, res, next) => {
+  // Déstructuration du email et du password
   const { email, password } = req.body;
   try {
+
+    // Vérification du email. S'il existe, le récupérer, sinon envoi message d'erreur
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, 'User not found !'));
+
+    // Si l'email existe, vérification du password en utilisant bcrypt. Si le password existe, on retourne true. Sinon, message d'erreur
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials !'));
+
+    // Si email & password existent, on crée un token en utilisant le user id attribué par MongoDB et une variable d'environnement SECRET key
     const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
+
+    // Sauvegarde du token dans un cookie. On retourne en confirmation le "rest" qui est toutes les infos du validUser sauf le password
     res
       .cookie('access_token', token, { httpOnly: true })
       .status(200)
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,7 +3,9 @@ import { signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
+// Création de la route /signup qui fera appel à la fonction signup crée dans la page auth.controller.js
 router.post('/signup', signup);
+// Création de la route /signin qui fera appel à la fonction signin crée dans la page auth.controller.js
 router.post('/signin', signin);
 
 export default router;
