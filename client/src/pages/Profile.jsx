23274696b7e5e5e99bee713b3ef6602919fb214e
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-01-18 11:26:43 +0100

    complete profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
 
 export default function Profile() {
+  const {currentUser} = useSelector((state) => state.user)
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img src={currentUser.avatar} alt='profile' className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
+        <input type='text' placeholder='username' id='username' className='border p-3 rounded-lg'/>
+        <input type='email' placeholder='email' id='email' className='border p-3 rounded-lg'/>
+        <input type='text' placeholder='password' id='password' className='border p-3 rounded-lg'/>
+        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>update</button>
+      </form>
+      <div className='flex justify-between mt-10'>
+        <button className='text-white bg-red-600 rounded-lg p-3 font-semibold hover:opacity-85'>Delete account</button>
+        <button className='text-slate-700 bg-white rounded-lg p-3 font-semibold hover:opacity-85'>Sign out</button>
+      </div>
+    </div>
   )
 }
