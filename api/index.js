ccc953013163976d1b29058a5f4578ad4f449833
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-01-19 11:24:16 +0100

    create user update api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 import userRouter from "./routes/user.route.js";
@@ -19,10 +20,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
-  }
-);
+});
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
