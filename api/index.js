c46161e46b00374cca4fae00018b6fab0f65edc6
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-01-22 20:19:26 +0100

    create get user listings api route

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000.');
 });
 
 app.use('/api/user', userRouter);
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -19,8 +19,12 @@ export default function OAuth() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ name: result.user.displayName, email: result.user.email, photo: result.user.photoURL }),
-      })
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
       const data = await res.json();
       dispatch(signInSuccess(data));
       navigate('/');
@@ -29,6 +33,12 @@ export default function OAuth() {
     }
   };
   return (
-    <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Continue with Google</button>
-  )
+    <button
+      onClick={handleGoogleClick}
+      type='button'
+      className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
+    >
+      Continue with Google
+    </button>
+  );
 }
