05d08d7732301b9d4b9a883258e517cb9bbc6e04
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2024-01-26 10:02:31 +0100

    add pre installation

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 import userRouter from "./routes/user.route.js";
@@ -17,6 +18,8 @@ mongoose
     console.log(err);
   });
 
+  const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -30,6 +33,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 // middleware to handle possible errors
 app.use((err, req, res, next) => {
